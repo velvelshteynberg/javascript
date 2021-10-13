9f04520c8b2ab3f7a600d0f403706fad9b97df2c
velvelshteynberg <velvelshteynberg@outlook.com>
2021-10-13 17:03:53 -0400

    finished first DOM assignment

--- section7-workingWithTheDOM/assignment/assignment.js
+++ section7-workingWithTheDOM/assignment/assignment.js
@@ -1,9 +1,14 @@
 let task1 = document.querySelector('#task-1');
-let tas1 = document.querySelector('li:first-of-type');
+let tas1 = document.querySelector('li');
 console.dir(tas1)
 tas1.style.backgroundColor = "black";
-task1.style.textColor = 'white';
+task1.style.Color = 'white';
 
-let title = document.getElementsByTagName('title');
-console.dir(title);
+let title = document.querySelector('title');
 title.textContent = 'Assignment-Solved';
+
+const title2 = document.head.querySelector('title');
+//title2.textContent = 'THe assignment is solved';
+
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment-Solved';
