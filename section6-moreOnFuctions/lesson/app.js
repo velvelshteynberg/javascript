bc4893129bc6580530dae447ad615763a67744d3
velvelshteynberg <velvelshteynberg@outlook.com>
2021-10-13 11:33:03 -0400

    assignment completed

--- section6-moreOnFuctions/lesson/app.js
+++ section6-moreOnFuctions/lesson/app.js
@@ -82,19 +82,33 @@ startGameBtn.addEventListener('click',  () => {
 
 //not related to game
 
-const sumUp = (resultHandler, ...number) => {
+const combine = (resultHandler,operation,  ...number) => {
     const validateNumber = (number) => {
 return isNaN(number) ? 0 : number
     }
+    
     let sum = 0;
     for (const num of number) {
-        sum += validateNumber(num);
+        if (operation === 'ADD') {
+            sum += validateNumber(num);
+        } else {
+            sum -= validateNumber(num);
+        }   
     }
     resultHandler(sum)
 };
 
-const showResult = (result) => {
-    alert('The result after adding all numbers is : ' + result);
+const subtractUp = function (resultHandler, ...numbers) {
+    let sum = 0;
+    for (const num of numbers) {
+        sum -= num;
+    }
+    resultHandler(sum);
 }
 
-sumUp(showResult,1,2,2,2,2,2,0,9,8,7,6,5,5,5,2,13,15,2,2);
+const showResult = (messageText, result) => {
+    alert(messageText + ' ' + result);
+}
+
+combine(showResult.bind(this, 'The result after addition is:'), 'ADD', 2, 2);
+combine(showResult.bind(this, 'The result after subtraction is:'),'SUBTRACT', 1,2,3,4,4)
