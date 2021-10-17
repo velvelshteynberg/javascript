594609d42961f16dc1e0e24b8b1b3501f0efd276
velvelshteynberg <velvelshteynberg@outlook.com>
2021-10-17 10:27:20 -0400

    working on iterables

---
+++ section8-arraysAndIterables/lesson/app.js
@@ -0,0 +1,29 @@
+/*const numbers = [1, 2, 3,];
+
+/*const moreNumbers = new Array('hi', 'welcome!');
+
+const aray = Array('great');
+
+const yetMoreNumbers = Array.of(2,3)*/
+
+/*const moreNumbers = Array.from('greatness');
+console.log(moreNumbers);
+
+const hobbies = ['sports']
+hobbies.push('reading'); //adds element at the end of the array
+hobbies.unshift('playing'); //adds element at the beginning of all the elements
+hobbies.pop(); //removes last items of array
+hobbies.shift();// removes first item of array
+hobbies[1] = 'coding'; // changed index 1 to coding
+hobbies[79] = 'practicing'; //this adds something to that position and all items in between are undefined but do exist
+
+hobbies.splice(0, 0, '14');*/
+
+/*const testResults = [1, 5.3, 1.5, 10.99];
+
+const storedResults = testResults.concat([3.55]);
+console.log(storedResults)
+
+console.log(testResults.indexOf(10.99));
+
+console.log(testResults.includes(5.3));*/
