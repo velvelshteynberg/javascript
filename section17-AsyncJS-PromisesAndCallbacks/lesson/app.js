fd99100bacede5ca95bf60bbca49cace17af0a0d
velvelshteynberg <velvelshteynberg@outlook.com>
2021-11-11 18:19:14 -0500

    working on section

--- section17-AsyncJS-PromisesAndCallbacks/lesson/app.js
+++ section17-AsyncJS-PromisesAndCallbacks/lesson/app.js
@@ -1,8 +1,25 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Done');
+    }, duration);
+  });
+  return promise;
+}
+
 function trackUserHandler() {
   console.log('Clicked!');
 }
 
 button.addEventListener('click', trackUserHandler);
+
+let result = 0;
+for (let i = 0; i < 1000000; i++){
+  result += i;
+}
+console.log(result);
+
+const promise  = new Promise()
