8fc1d2a35b597dc4265ef3b3ec2a0a395f9cc1ed
velvelshteynberg <velvelshteynberg@outlook.com>
2021-11-16 10:11:40 -0500

    finished promise section: concepts fully clear: implementation not 100% clear

--- section17-AsyncJS-PromisesAndCallbacks/lesson/app.js
+++ section17-AsyncJS-PromisesAndCallbacks/lesson/app.js
@@ -1,62 +1,70 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      success => {
+        resolve(success);
+      },
+      error => {
+        reject(error);
+      },
+      opts
+    );
+  });
+  return promise;
+};
+
 const setTimer = (duration) => {
   const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('Done');
+    setTimeout(() => {
+      resolve('Done!');
     }, duration);
   });
   return promise;
-}
+};
 
-function trackUserHandler() {
-  console.log('Clicked!');
+async function trackUserHandler() {
+  let posData;
+  let timerData;
+
+  try {
+     posData = await getPosition(); //this doesnt stop script execution it just wraps the entire code in a promise
+    timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
+    console.log(timerData, posData);
+    // .then((posData) => {
+    //   positionData = posData;
+    //   return setTimer(2000);
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    //   return 'on we go...';
+    // })
+    // .then((data) => {
+    //   console.log(data, positionData);
+    // });
+  // setTimer(1000).then(() => {
+  //   console.log('Timer done!');
+  // // });
+  // console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
 
-let result = 0;
-for (let i = 0; i < 1000000; i++){
-  result += i;
-}
-console.log(result);
+// let result = 0;
 
+// for (let i = 0; i < 100000000; i++) {
+//   result += i;
+// }
 
-let promis = new Promise((resolve, reject) => {
-  if (!false) {
-  reject('FALSE');
-  } else {
-resolve('SUCCESS');
-  }
-  
-  
-})
- 
-promis.then(message  => {
-  console.log(message);
-}).catch(message => {
-  console.log(message);
-})
+// console.log(result);
 
+// Promise.race([getPosition(), setTimer(1000)]).then(data=>console.log(data))  race returns the first executed promise
+// Promise.all([getPosition(), setTimer(1000)]) ex ecutes all promises
+  // .then(promise => console.log(promise))
 
-const theLocation = new Promise((resolve, reject) => {
-  navigator.geolocation.getCurrentPosition(success => {
-    resolve(success);
-  }, failure => {    
-    reject(failure);
-  });
-})
-
-theLocation
-  .then(message => {
-    console.log(message)
-    return message
-  })
-  .then(message => {
-    return message.timestamp;
-  })
-  .then(message => {
-    console.log(message.toString().length);
-  })
-  .catch(err => console.log(err.message))
+Promise.allSettled([getPosition(), setTimer(500)]).then(data => console.log(data));
