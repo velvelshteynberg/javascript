df68ff9114fefb81dc721eb46600b64a4063eb6c
velvelshteynberg <velvelshteynberg@outlook.com>
2021-11-17 10:12:25 -0500

    Starting Http request sessions

---
+++ section18-HTTPRequests/lesson/assets/scripts/app.js
@@ -0,0 +1,57 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
+
+function sendHTTPRequests(method, url, data) {
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = 'json';
+        xhr.onload = function () {
+            resolve(xhr.response);
+        };
+        xhr.send(JSON.stringify(data));
+        }) 
+    return promise;
+    };
+
+async function fetchPosts() {
+    const responseData = await sendHTTPRequests(
+        'GET',
+        'https://jsonplaceholder.typicode.com/posts'
+    );
+        const listOfPosts = responseData;
+        for (const post of listOfPosts) {
+            const postEl = document.importNode(postTemplate.content, true);
+            postEl.querySelector('h2').textContent = post.title.toUpperCase();
+            postEl.querySelector('p').textContent = post.body;
+            listElement.append(postEl);
+        }
+};
+
+async function createPost(title, content) {
+    const userId = Math.random();
+    const post = {
+        Title: title,
+        body: content,
+        userId: userId
+    };
+
+    sendHTTPRequests(
+        'POST',
+        'https://jsonplaceholder.typicode.com/posts',
+        post
+    );
+}
+
+fetchButton.addEventListener('click', fetchPosts);
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log(event);
+    const enteredTitle = event.currentTarget.querySelector('#title').value;
+    const enteredContent = event.currentTarget.querySelector('#content').value;
+    createPost(enteredTitle, enteredContent)
+})
+createPost('Dummy', 'A dummy post!');
